6ed530f6e63071f97564e0fb10fbd9834395b18d
Maria Clara L. Monteiro <62342158+mariaclara31@users.noreply.github.com>
2020-03-28 19:23:17 -0300

    Add files via upload

---
+++ cadastrar.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+ob_start();
+$btnCadUsuario = filter_input(INPUT_POST, 'btnCadUsuario', FILTER_SANITIZE_STRING);
+if($btnCadUsuario){
+	include_once 'conexao.php';
+	$dados_rc = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+	$erro = false;
+
+	$dados_st = array_map('strip_tags', $dados_rc);
+	$dados = array_map('trim', $dados_st);
+
+	if(in_array('',$dados)){
+		$erro = true;
+		$_SESSION['msg'] = "Necessário preencher todos os campos";
+	}elseif((strlen($dados['senha'])) < 6){
+		$erro = true;
+		$_SESSION['msg'] = "A senha deve ter no minímo 6 caracteres";
+	}elseif(stristr($dados['senha'], "'")) {
+		$erro = true;
+		$_SESSION['msg'] = "Caracter ( ' ) utilizado na senha é inválido";
+	}else{
+		$result_usuario = "SELECT id FROM usuarios WHERE usuario='". $dados['usuario'] ."'";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)){
+			$erro = true;
+			$_SESSION['msg'] = "Este usuário já está sendo utilizado";
+		}
+
+		$result_usuario = "SELECT id FROM usuarios WHERE email='". $dados['email'] ."'";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)){
+			$erro = true;
+			$_SESSION['msg'] = "Este e-mail já está cadastrado";
+		}
+	}
+
+
+	//var_dump($dados);
+	if(!$erro){
+		//var_dump($dados);
+		$dados['senha'] = password_hash($dados['senha'], PASSWORD_DEFAULT);
+
+		$result_usuario = "INSERT INTO usuarios (nome, email, setor, usuario, senha) VALUES (
+						'" .$dados['nome']. "',
+						'" .$dados['email']. "',
+						'" .$dados['setor']. "',
+						'" .$dados['usuario']. "',
+						'" .$dados['senha']. "'
+						)";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if(mysqli_insert_id($conn)){
+			$_SESSION['msgcad'] = "Usuário cadastrado com sucesso";
+			header("Location: login.php");
+		}else{
+			$_SESSION['msg'] = "Erro ao cadastrar o usuário";
+		}
+	}
+
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Cadastro</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/signin.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="form-signin" style="background: #8A2BE2;">
+				<img src="logo.png" class="img-fluid img-thumbnail">
+				<strong><h2>Cadastrar Usuário</h2></strong>
+				<?php
+					if(isset($_SESSION['msg'])){
+						echo $_SESSION['msg'];
+						unset($_SESSION['msg']);
+					}
+				?>
+				<form method="POST" action="">
+					<!--<label>Nome</label>-->
+					<input type="text" name="nome" placeholder="Digite seu nome completo" class="form-control"><br>
+
+					<!--<label>E-mail</label>-->
+					<input type="text" name="email" placeholder="Digite o seu e-mail" class="form-control"><br>
+
+					<!--<label>Setor</label>-->
+					<input type="text" name="setor" placeholder="Digite o seu setor" class="form-control"><br>
+
+					<!--<label>Usuário</label>-->
+					<input type="text" name="usuario" placeholder="Digite sua matrícula" class="form-control"><br>
+
+					<!--<label>Senha</label>-->
+					<input type="password" name="senha" placeholder="Digite a senha" class="form-control"><br>
+
+					<input type="submit" name="btnCadUsuario" value="Cadastrar" class="btn btn-success"><br><br>
+
+					<div class="row text-center" style="margin-top: 20px;">
+						<strong>Lembrou? <a href="login.php">Clique aqui</a> para logar</strong>
+					</div>
+				</form>
+			</div>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
+
+		<title>Login</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/signin.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="form-signin" style="background: #D8BFD8;">
+				<img src="logo.png" class="img-fluid img-thumbnail">
+				<h2>Área restrita</h2>
+				<?php
+					if(isset($_SESSION['msg'])){
+						echo $_SESSION['msg'];
+						unset($_SESSION['msg']);
+						}
+					if(isset($_SESSION['msgcad'])){
+						echo $_SESSION['msgcad'];
+						unset($_SESSION['msgcad']);
+						}
+				?>
+					<form method="POST" action="valida.php">
+						<label>Usuário</label>
+						<input type="text" name="usuario" placeholder="Digite seu matrícula" class="form-control"><br><br>
+
+						<label>Senha</label>
+						<input type="password" name="senha" placeholder="Digite a sua senha"class="form-control"><br><br>
+
+						<input type="submit" name="btnLogin" value="Acessar"class="btn btn-success"><br><br>
+
+						<h4>Você ainda não possui uma conta?</h4>
+						<a href="cadastrar.php">Cadastre-se</a>
+					</form>
+				</div>
+			</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
