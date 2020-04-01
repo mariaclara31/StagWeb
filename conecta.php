443cfe48b48021e3ebbf6e17bb54bc9f202e6b21
Maria Clara L. Monteiro <62342158+mariaclara31@users.noreply.github.com>
2020-04-01 18:59:32 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -9,7 +9,7 @@ $dbname = "ponto";
 //Cria a conexão
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
-$result_horario = "INSERT INTO `horario`(`Entrada`, `Almoço`, `Retorno`, `Saída`) VALUES ('$posicao')";
+$result_horario = "INSERT INTO `horario`(`Entrada`, `Almoço`, `Retorno`, `Saída`) VALUES ("$posicao")";
 $resultado_horario = mysqli_query($conn, $result_horario);
 ?>
 
